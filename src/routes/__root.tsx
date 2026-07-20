db9418243fed384f220617431de07f26f633795c
akansha20204-netizen <akansha20204@gmail.com>
2026-07-20 23:14:06 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -126,6 +126,15 @@ function RootShell({ children }: { children: ReactNode }) {
      crossorigin="anonymous"></script>
       <head>
         <HeadContent />
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NRCFQQJXYX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NRCFQQJXYX');
+</script>
            </head>
       <body>
         {children}
